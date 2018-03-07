e9ae3e9ea1e0d6d933c2306d9eeebf1d46881185
Ken Stanley <ken@duboisproject.com>
2018-03-07 08:52:07 -0500

    Trivial changes to the regression test. There is an unexplained anomaly in the regression output, somehow the order of a few lines changed. As if there was an asychronous write involved or a change to the inuput files which I don't remberm making. Since all this happened this morning, my memory should be solid and also verifiable by looking at the git log. If it is asynchronous, it does not repeat. It now gives the lines in the same order each time.

---
+++ regressAddColumns.sh
@@ -0,0 +1,9 @@
+#
+#
+#
+rm shorterMergedOutput.csv
+node MatchVoterIDandAddColumns.js ShorterLargeFile.csv regressFileOfVols shorterMergedOutput.csv
+echo ""
+echo "And now we diff this file with what we got on the last regression run"
+echo ""
+diff shorterMergedOutput.csv regressShorterMergedOutput.csv
