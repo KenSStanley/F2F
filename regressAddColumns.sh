395298b5752736b20ce46366fd7aa518a667b6d2
Ken Stanley <ken@duboisproject.com>
2018-03-07 11:59:41 -0500

    Make regressAddColumns.sh run from another directory

--- regressAddColumns.sh
+++ regressAddColumns.sh
@@ -1,8 +1,8 @@
 #
 #
 #
-rm shorterMergedOutput.csv
-node MatchVoterIDandAddColumns.js ShorterLargeFile.csv regressFileOfVols shorterMergedOutput.csv
+rm -f shorterMergedOutput.csv
+node /Users/kenstanley/GitRepository/github/F2F/MatchVoterIDandAddColumns.js ShorterLargeFile.csv regressFileOfVols shorterMergedOutput.csv
 echo ""
 echo "And now we diff this file with what we got on the last regression run"
 echo ""
