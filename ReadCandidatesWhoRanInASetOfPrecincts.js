7188e92d128a6c294024613922a5fb04f10c3cee
Ken Stanley <ken@500sw.com>
2018-03-21 12:16:02 -0400

    remove parseLargeFile from ReadCandidatesWhoRanInASetOfPrecincts.js

--- ReadCandidatesWhoRanInASetOfPrecincts.js
+++ ReadCandidatesWhoRanInASetOfPrecincts.js
@@ -31,24 +31,6 @@ const writeOneLine = (entry) => {
 let inputFileName = "Precinct2016All"
 let outputFileName = "GibbsPrecincts" ;  
 
-//function to parse the large file and store name details to memory
-const parseLargeFile = function(data) {
-  let rows = data.split("\n");
-  //iterate each line and parse the data
-  for (let line = 0; line < rows.length; line++) {
-    if (rows[line].trim().replace(/\r?\n?/g, '')) {
-      let splitRow = rows[line].split(',');
- 
-      let precinctID = splitRow[0] + splitRow[1] + splitRow[2]; 
-
-      let nameObject = {
-        precinctID: precinctID,
-      };
-      writeOneLine( nameObject ) ; 
-    }
-  }
-};
-
 
 // parse input arguments
 process.argv.forEach(function (val, index, array) {
@@ -97,7 +79,6 @@ const init = function() {
           candidates.set(candidate,candidate); 
       }
   }
-//  parseLargeFile(inputFileContents) ;
   headers = "Candidate Name\n" ;
   writeOutput(headers);
 /*
