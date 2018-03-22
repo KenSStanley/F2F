e9eb219089def51682b31dc77ae9860efe5eb5c1
Ken Stanley <ken@500sw.com>
2018-03-22 08:33:03 -0400

    Minor cleanup to grabDataForSelectPrecincts.sh, remove files before running the javascript

--- grabDataForSelectPrecincts.sh
+++ grabDataForSelectPrecincts.sh
@@ -2,9 +2,18 @@
 #  This script grabs data for select precincts and creates a csv file for them.  
 #
 #  npm install collections
+#   tr '\r' '\n' < 2012precinct.txt  > 2012precinctLF2.txt converts <CR> to  <LF>
+#   egrep -E "Ashland|Coshocton|Holmes|Huron|Knox|Knox|Lorain|Medina|Richland|Stark|Tuscarawas" 2012precinctLF2.txt >GibbsCounties2012.txt
+#  https://github.com/openelections/openelections-data-oh has alot of data
 #
 #  Compute GibbsPrecincts
+rm GibbsPrecincts
 node readPrecinctFromStatePrecinctFile.js Precinct2016Gibbs GibbsPrecincts
 #  Compute GibbsCandidates
+rm GibbsCandidates
 node ReadCandidatesWhoRanInASetOfPrecincts.js Precinct2016All GibbsPrecincts GibbsCandidates
-node collectDataForASetOfPrecincts.js Precinct2016Gibbs GibbsPrecincts 2016OH7Candidates
+#  cp GibbsCandidates 2016OH7Candidates
+#  vi 2016OH7Candidates - delete all but those candidates whom we care the most about
+#
+#  
+node collectDataForASetOfPrecincts.js Precinct2016All GibbsPrecincts 2016OH7Candidates voteTotalsForAllGibbsPrecictsAndCandidates 
