ba32b0381f1b7cde991cb5531e71440f6337507d
Ken Stanley <ken@500sw.com>
2018-06-13 12:10:26 -0400

    Add precincts MAN 4 - B and MAN 6 - B to collectDemocraticMansfieldPrecincts.sh

--- collectDemocraticMansfieldPrecincts.sh
+++ collectDemocraticMansfieldPrecincts.sh
@@ -1,3 +1,3 @@
 cp VoterFileHeader.csv MansfieldDemPrecincts.csv
-egrep -E "MAN 4 - C|MAN 6 - D|MAN 6 - A|MAN 5 - B|MAN 4 - E|MAN 4 - D|MAN 3 - A" Mansfield.txt >>MansfieldDemPrecincts.csv
+egrep -E "MAN 4 - C|MAN 6 - D|MAN 6 - A|MAN 5 - B|MAN 4 - B|MAN 6 - B|MAN 4 - E|MAN 4 - D|MAN 3 - A" Mansfield.txt >>MansfieldDemPrecincts.csv
 
