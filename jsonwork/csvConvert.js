746a715e9e41277331094f66e85bcf4f7023c362
Keith Swenson <github@kswenson.oib.com>
2018-06-29 00:01:28 -0700

    reworked, cleaned up data handling and converting data cases

--- jsonwork/csvConvert.js
+++ jsonwork/csvConvert.js
@@ -1,38 +1,38 @@
 //CSV to JSON converter
 const fs = require('fs');
+const utils = require("./utils.js");
+
+convertFile("Mansfield6A  - Turf 1 MAN 6A", "Turf1", "Man6a", convertInternal);
+convertFile("Mansfield6A  - Turf 2 MAN 6A", "Turf2", "Man6a", convertInternal);
+convertFile("Mansfield6A  - Turf 3 MAN 6A", "Turf3", "Man6a", convertInternal);
+convertFile("Mansfield6A  - Turf 4 MAN 6A", "Turf4", "Man6a", convertInternal);
+convertFile("Mansfield6A  - Turf 5 MAN 6A", "Turf5", "Man6a", convertInternal);
+convertFile("Mansfield6A  - Turf 6 MAN 6A", "Turf6", "Man6a", convertInternal);
+convertFile("Mansfield6A  - Turf 7 MAN 6A", "Turf7", "Man6a", convertInternal);
+convertFile("Mansfield6A  - Turf 8 MAN 6A", "Turf8", "Man6a", convertInternal);
+convertFile("Mansfield6A  - Turf 9 Apts", "Turf9", "Man6a", convertInternal);
+
+//convertFile("Mansfield voters in 9 Dem Precincts - In 24June18Not in23DEC17","unknown", "Man6a", convertOrthodox);
 
 
-convertFile("Mansfield6A  - Turf 8 MAN 6A", "Turf8", "Man6a");
-convertFile("Mansfield6A  - Turf 7 MAN 6A", "Turf7", "Man6a");
-convertFile("Mansfield6A  - Turf 6 MAN 6A", "Turf6", "Man6a");
-convertFile("Mansfield6A  - Turf 5 MAN 6A", "Turf5", "Man6a");
-convertFile("Mansfield6A  - Turf 4 MAN 6A", "Turf4", "Man6a");
-convertFile("Mansfield6A  - Turf 3 MAN 6A", "Turf3", "Man6a");
-convertFile("Mansfield6A  - Turf 2 MAN 6A", "Turf2", "Man6a");
-convertFile("Mansfield6A  - Turf 1 MAN 6A", "Turf1", "Man6a");
-convertFile("Mansfield6A  -  Mansfield6A Original", "unknown", "Man6a");
-
-//convertFile("Mansfield voters in 9 Dem Precincts - In 24June18Not in23DEC17","unknown", "Man6a");
-//convertFile("Mansfield voters in 9 Dem Precincts - Sheet4","unknown", "Man6a");
-
-function convertFile(fileName, thisTurf, precinct) {
+function convertFile(fileName, thisTurf, precinct, recordConverter) {
     var csvContents = fs.readFileSync(fileName+".csv", 'utf8');
 
     var csvRows = csvContents.split("\n");
 
     var nameRow = csvRows[0];
-    var names = parseLine(nameRow);
+    var names = utils.csvParseLine(nameRow);
     var total = [];
 
     for (var i=1; i<csvRows.length; i++) {
         console.log("###################################."+i)
-        var vals = parseLine(csvRows[i]);
+        var vals = utils.csvParseLine(csvRows[i]);
         if (!vals[0]) {
             continue;
         }
-        var o = {};
-        o.turf=thisTurf;
-        o.precinct = precinct;
+        var approx = {};
+        approx.turf=thisTurf;
+        approx.precinct = precinct;
         var limit = vals.length;
         if (limit>names.length) {
             limit = names.length;
@@ -52,159 +52,105 @@ function convertFile(fileName, thisTurf, precinct) {
                 console.log("  "+oneName+":  "+oneVal);
                 continue;
             }
-            if (!tackableField(oneName)) {
-                console.log("  "+oneName+":  "+oneVal);
-                continue;
-            }
             console.log("- "+oneName+":  "+oneVal);
-            if (oneName == "Name_Age_Address") {
-                var parenOpen = oneVal.indexOf("(");
-                var parenClose = oneVal.indexOf(")");
-                o.name = oneVal.substring(0,parenOpen).trim();
-                o.age = oneVal.substring(parenOpen+1, parenClose).trim();
-                o.shortAddress = oneVal.substring(parenClose+1).trim();
-            }
-            else {
-                o[oneName] = oneVal.trim();
-            }
+            approx[oneName] = oneVal.trim();
         }
-        if ((!o.name) && o.firstName && o.lastName) {
-            o.name = o.firstName +" "+ o.lastName;
-            console.log("Converting name: "+o.name+"-"+o.firstName+"-"+o.lastName);
-            delete o.firstName;
-            delete o.lastName;
-        }
-        if ((!o.age) && o.bday) {
-            var dashPos = o.bday.indexOf("-");
-            var year = Number(o.bday.substring(0,dashPos));
-            o.age = 2018-year;
-            console.log("Converting bday: "+o.bday+" - "+o.age);
-            delete o.bday;
-        }
-        if (o.precinctf) {
-            o.precinct = convertPrecinct(o.precinctf);
-            console.log("Converting precinct: ("+o.precinctf+") - "+o.precinct);
-            delete o.precinctf;
-        }
-        total.push(o);
-        
+        recordConverter(total,approx,thisTurf, precinct);
     }
     fs.writeFileSync(fileName+".json", JSON.stringify(total,null, 2));
 }
 
-function parseLine(val) {
-    var i=0;
-    var ch = val[i++];
-    while (ch == '\r') {
-        //ignore linefeed characters wherever they are, particularly just before end of file
-        if (i>=val.length) {
-            return null;
-        }
-        ch = val[i++];
+
+
+function convertInternal(total, approx, thisTurf, precinct ) {
+    if (!approx.SOS_VOTERID || !approx.SOS_VOTERID.startsWith("OH")) {
+        return;
     }
-    if (i>=val.length) {
-        return null;
-    }
-    var res = [];
-    var curVal = "";
-    var inquotes = false;
-    while (i<val.length) {
-        if (inquotes) {
-            started=true;
-            if (ch == '\"') {
-                inquotes = false;
-            }
-            else {
-                curVal += ch;
-            }
-        }
-        else {
-            if (ch == '\"') {
-                inquotes = true;
-                if (started) {
-                    // if this is the second quote in a value, add a quote
-                    // this is for the double quote in the middle of a value
-                    curVal += '\"';
-                }
-            }
-            else if (ch == ',') {
-                res.push(curVal);
-                curVal = ""
-                started = false;
-            }
-            else if (ch == '\r') {
-                //ignore LF characters
-            }
-            else if (ch == '\n') {
-                //end of a line, break out
-                break;
-            }
-            else {
-                curVal += ch;
-            }
-        }
-        ch = val[i++];
-    }
+    var official = {};
+    official.sosId = approx.SOS_VOTERID;
+    var oneVal = approx.Name_Age_Address;
+    var parenOpen = oneVal.indexOf("(");
+    var parenClose = oneVal.indexOf(")");
+    official.name = oneVal.substring(0,parenOpen).trim();
+    official.age = oneVal.substring(parenOpen+1, parenClose).trim();
+    official.shortAddress = oneVal.substring(parenClose+1).trim();
     
-    res.push(curVal);
-    return res;
+    if (approx.precinct) {
+        official.precinct = approx.precinct;
+    }
+    official.mappableAddress = approx.mappableAddress;
+
+    official.longitude = Number(approx.longitude);
+    official.latitude  = Number(approx.latitude);
+    official.turf = thisTurf;
+    official.precinct = precinct;
+    official.order = Number(approx.order);
+    official.full_order = Number(approx.full_order);
+    total.push(official);
 }
 
-function tackableField(fieldName) {
-    if (fieldName=="SOS_VOTERID") {
-        return true;
+
+function convertOrthodox(total, approx, thisTurf, precinct ) {
+    var official = {};
+    official.sosId = approx.sosId
+    official.name = utils.capSentence(approx.firstName + " " + approx.lastName);
+    if (approx.bday) {
+        var dashPos = approx.bday.indexOf("-");
+        var year = Number(approx.bday.substring(0,dashPos));
+        official.age = 2018-year;
     }
-    if (fieldName=="longitude") {
-        return true;
+    else {
+        official.age = -1;
     }
-    if (fieldName=="latitude") {
-        return true;
+    if (approx.door) {
+        official.shortAddress = fixShortAddress(approx.door);
     }
-    if (fieldName=="order") {
-        return true;
+    else {
+        official.mappableAddress = "MISSING DOOR FROM DB";
     }
-    if (fieldName=="full_order") {
-        return true;
+    if (approx.precinct) {
+        official.precinct = convertPrecinct(approx.precinct);
     }
-    if (fieldName=="mappableAddress") {
-        return true;
+    if (approx.address) {
+        official.mappableAddress = makeMappableAddress(approx.address);
     }
-    if (fieldName=="shortAddress") {
-        return true;
+    else {
+        official.mappableAddress = "MISSING ADDRESS FROM DB";
     }
-    if (fieldName=="age") {
-        return true;
-    }
-    if (fieldName=="name") {
-        return true;
-    }
-    if (fieldName=="firstName") {
-        return true;
-    }
-    if (fieldName=="lastName") {
-        return true;
-    }
-    if (fieldName=="Name_Age_Address") {
-        return true;
-    }
-    if (fieldName=="bday") {
-        return true;
-    }
-    if (fieldName=="precinctf") {
-        return true;
-    }
-    return false;
+    official.longitude = Number(approx.Longitude);
+    official.latitude  = Number(approx.Latitude);
+    total.push(official);
 }
+
 
 function convertPrecinct(value) {
-    //if (value == "MAN 3 - A") {
-    //    return "Man3a"
-    //}
-    //if (value == "MAN 6 - A") {
-    //    return "Man6a"
-    //}
+    //convert "MAN 3 - A" to "Man3a"
     var stripped = value.replace(/ /g, "").replace("-", "");
-    return stripped.substring(0,1) + stripped.substring(1).toLowerCase();
+    return utils.capWord(stripped);
 }
+
+function fixShortAddress(value) {
+    //first get rid of unnecessary quotes,
+    //then trim to eliminate unneeded spaces off end.
+    return utils.capSentence(value.replace(/\"/g, "").trim());
+}
+function makeMappableAddress(address) {
+    //first get rid of 1/2 if it is there
+    var idx = address.indexOf("1/2");
+    if (idx>0) {
+        address = address.substring(0,idx)+address.substring(idx+4);
+    }
+    return utils.capSentence(address).trim()+" Manfield OH";
+}
+
+
+function stripZipCode(address) {
+    //first get rid of unnecessary quotes,
+    //then trim to eliminate unneeded spaces off end.
+    return value.substring(0, value.length-5).trim();
+}
+
+
+
 
 console.log("ALL DONE");
